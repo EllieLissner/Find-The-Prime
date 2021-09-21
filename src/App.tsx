84e640de718a8bf1955c538d57bfbeed6e7546b9
Ellie-Lissner <eswalker0723@gmail.com>
2021-09-21 17:41:47 -0500

    updated readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ChangeEvent, MouseEvent, useState } from 'react'
 
 function App() {
+
+  const[number, setNumber] = useState('')
+
+
+  const handlePrimeFinder = (e: MouseEvent , num: number) => {
+    e.preventDefault()
+    let int = Number(num)
+    
+    for (let i = int; i > 0 ; i-- ) {
+      console.log(100 - int)
+    }
+    
+  }
+
+  function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
+    setNumber(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <div className="header">Find the Prime</div>
+      <form className="input">
+        <label>nth Prime #</label>
+        <input 
+          type="number" 
+          name="num"
+          value={number}
+          onChange={handleInputChange}
+        /> 
+        <div>1-100</div>
+      <button onClick={(e) => handlePrimeFinder(e, Number(number))}>
+        Pick a Number
+      </button>
+      </form>
     </div>
   );
 }
